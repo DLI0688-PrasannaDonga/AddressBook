5a2f57e41a7b5e274b5105fd6c80ff991f945fd5
Prasanna Donga <Prasanna.Donga@godigit.com>
2024-07-25 17:33:58 +0530

    add person details

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,6 +1,6 @@
 public class Contacts {
     String firstName;
-    String LastName;
+    String lastName;
     String address;
     String city;
     String state;
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to address book");
+        Scanner sc= new Scanner(System.in);
+        Contacts con = new Contacts();
+        System.out.println("Enter your FirstName: ");
+        con.firstName=sc.nextLine();
+        System.out.println("Enter your LastName: ");
+        con.lastName=sc.nextLine();
+        System.out.println("Enter your Address: ");
+        con.address=sc.nextLine();
+        System.out.println("Enter your City: ");
+        con.city=sc.nextLine();
+        System.out.println("Enter your State: ");
+        con.state=sc.nextLine();
+        sc.nextLine();
+        System.out.println("Enter your Zip: ");
+        con.zip=sc.nextLong();
+        System.out.println("Enter your PhoneNumber: ");
+        con.phoneNumber=sc.nextLong();
+        System.out.println("Enter your Email: ");
+        con.email=sc.nextLine();
+
+
 
     }
 }
