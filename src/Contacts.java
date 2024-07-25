1b90c6f24ec14a37e0588d77fecd7aa9c8aefacb
Prasanna Donga <Prasanna.Donga@godigit.com>
2024-07-25 16:52:30 +0530

    adding Contacts class

---
+++ src/Contacts.java
@@ -0,0 +1,10 @@
+public class Contacts {
+    String firstName;
+    String LastName;
+    String address;
+    String city;
+    String state;
+    long zip;
+    long phoneNumber;
+    String email;
+}
